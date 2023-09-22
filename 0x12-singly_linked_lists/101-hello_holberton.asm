d9665653e1fd7db751a9d3a6e5323ee6b2b16ef4
Kyere <rnstkr@gmail.com>
2023-09-22 15:57:43 -0700

    101-hello_holberton.asm

--- 0x12-singly_linked_lists/101-hello_holberton.asm
+++ 0x12-singly_linked_lists/101-hello_holberton.asm
@@ -1,26 +1,9 @@
-section .data
-    hello db 'Hello, Holberton',0x0A  ; The string to print, followed by a newline character
-
-section .text
-    global main
-
+global   main
+	  extern    printf
 main:
-    ; Prepare the arguments for the printf function
-    mov rdi, hello  ; Load the address of the format string into rdi
-    mov rax, 0      ; The syscall number for sys_write (printf uses write)
-    call printf     ; Call the printf function
-
-    ; Exit the program
-    mov rax, 60     ; The syscall number for sys_exit
-    xor rdi, rdi    ; Exit status: 0
-    syscall
-
-section .bss
-    dummy resb 1    ; A dummy variable to satisfy the printf call
-
-; Printf function
-printf:
-    ; Inputs:
-    ;   rdi: Pointer to the format string
-    ; Outputs:
-    ;   None (rax is clobbered)
+	  mov   edi, format
+	  xor   eax, eax
+	  call  printf
+	  mov   eax, 0
+	  ret
+format: db `Hello, Holberton\n`,0x0A  ; The string to print
