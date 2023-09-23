e3e384e1c2e19e74ead2ff2713053394950d7790
Kyere <rnstkr@gmail.com>
2023-09-23 15:39:28 -0700

    101-hello_holberton.asm

--- 0x12-singly_linked_lists/101-hello_holberton.asm
+++ 0x12-singly_linked_lists/101-hello_holberton.asm
@@ -1,9 +1,9 @@
-format: db 'Hello, Holberton\n',0
-	extern printf
-	global main
+	global   main
+	  extern    printf
 main:
 	  mov   edi, format
 	  xor   eax, eax
 	  call  printf
 	  mov   eax, 0
 	  ret
+format: db `Hello, Holberton\n`,0
