d25e646579cf2e2188c7f5196fe74dc2353a41a2
Kyere <rnstkr@gmail.com>
2023-09-23 15:10:07 -0700

    101-hello_holberton.asm

--- 0x12-singly_linked_lists/101-hello_holberton.asm
+++ 0x12-singly_linked_lists/101-hello_holberton.asm
@@ -1,14 +1,23 @@
 section .data
 	hello db 'Hello, Holberton\n', 0 ; The string to be printed
 	format db "%s",0 ;format specifier for printf,null-terminated
+
 section .text
 	extern printf
 	global main
 main:
-	push rbp ; It saves the base pointer
-	mov rdi, format ;The format string
-	mov rsi, hello ;Load the address of the string
-	call printf ; call the printf function
-	pop rbp     ;To restore the base pointer
-	xor rax,rax ;Set return value to 0 for success
-	ret
+	; Save the base pointer and align the stack to a 16-byte boundary
+	push rbp
+	mov rbp, rsp
+	sub rsp, 8
+
+	; Prepare the arguments for printf
+	mov rdi, format ; The format string
+	mov rsi, hello ; Load the address of the string
+
+	; Call printf
+	call printf
+
+	; Restore the stack and the base pointer
+	add rsp, 8
+	pop rbp
