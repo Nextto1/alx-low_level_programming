c5cbf63e54c7f1ba458d027e326c1971f92bf749
Kyere <rnstkr@gmail.com>
2023-09-25 15:31:01 -0700

    0-print_listint.c

--- 0x13-more_singly_linked_lists/0-print_listint.c
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -6,17 +6,16 @@
  *
  * Return: number of nodes
  */
-
 size_t print_listint(const listint_t *h)
 {
-	size_t count_node = 0;
+	size_t node_count = 0;
 
 	while (h)
 	{
 		printf("%d\n", h->n);
-		count_node++;
+		node_count++;
 		h = h->next;
 	}
 
-	return (count_node);
+	return (node_count);
 }
