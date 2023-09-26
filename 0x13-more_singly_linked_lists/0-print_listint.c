49d0554d34f28bd2e55d05321bf3e76033280828
Kyere <rnstkr@gmail.com>
2023-09-26 07:06:25 -0700

    0-print_listint.c

--- 0x13-more_singly_linked_lists/0-print_listint.c
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -4,7 +4,7 @@
 /**
  * print_listint - prints all the elements of the linked list
  * @h: linked list of type listint_t to print
- * Return: The number of nodes
+ * Return: the number of nodes
  */
 
 size_t print_listint(const listint_t *h)
